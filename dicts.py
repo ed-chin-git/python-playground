c31f91dc5a3aae2a68f7e74fbfae878656aa7f70
ed.chin.git <edgardochin@gmail.com>
2020-12-31 11:18:50 -0600

    Add Dict Exercises

--- dicts.py
+++ dicts.py
@@ -1,1 +1,16 @@
-# Based on https://youtu.be/HGOBQPFzWKo
+# Based on https://youtu.be/HGOBQPFzWKo?t=1790
+
+
+campN={}
+campN[(1,3,5)]=2
+campN[(3,2,1)]=6
+campN[(1,3)]=10
+
+sum=0
+for k in campN:
+    sum += campN[k]
+
+# print(len(campN)+sum)
+
+for i in campN:
+    print(i)

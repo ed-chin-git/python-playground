6eb3a2c4d2d7effcdd808fe062d8b561a83d4019
ed.chin.git <edgardochin@gmail.com>
2021-01-02 09:54:55 -0600

    Add collections exercises

--- collections.py
+++ collections.py
@@ -1,1 +1,13 @@
-# Based on https://youtu.be/HGOBQPFzWKo
+# Based on https://youtu.be/HGOBQPFzWKo?t=4970
+
+# COLLECTIONS 
+#  Counter, Namedtuple, OrderedDict,  defaultdict
+import utils_edchin.pyxlib as pyx 
+pyx = pyx()
+pyx.clear_terminal()
+
+from collections import Counter
+from collections import namedtuple
+from collections import OrderedDict
+from collections import defaultdict
+from collections import deque

9837ea217599f28a62e46c967d6df3f16b32c984
ed.chin.git <edgardochin@gmail.com>
2021-01-03 09:55:07 -0600

    Add python collections exercises

--- collections.py
+++ collections.py
@@ -1,13 +1,38 @@
+# COLLECTIONS 
+# Data structures : Counter, namedtuple, OrderedDict,  defaultdict, deque
 # Based on https://youtu.be/HGOBQPFzWKo?t=4970
 
-# COLLECTIONS 
-#  Counter, Namedtuple, OrderedDict,  defaultdict
 import utils_edchin.pyxlib as pyx 
 pyx = pyx()
 pyx.clear_terminal()
 
-from collections import Counter
+
+# COUNTER
+# container that stores the elements as dictionary keys
+# and their count as values
+from collections import Counter 
+list_1 = ["bill", "ed", "sam", "bill", "ted", "sam", "sam", "sam"]
+
+countr_1 = Counter(list_1)
+
+print('List :', list_1)
+print(countr_1)
+print(countr_1.items())
+print(countr_1.keys())
+print(countr_1.values())
+
+print('.most_ common', countr_1.most_common(1))
+print('.most_ common[key]', countr_1.most_common(1)[0][0])
+print('.most_ common[value]', countr_1.most_common(1)[0][1])
+print('.element returns an iterable:', countr_1.elements())
+print(list(countr_1.elements()))
+
+
+
 from collections import namedtuple
+
 from collections import OrderedDict
+
+
 from collections import defaultdict
 from collections import deque
